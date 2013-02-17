3a523e860a2e140b0a79451a9de3a6fb293903de
Eugene  Klimov <eklimov@rdm-soft.com>
2013-02-17 13:56:17 +0600

    deploy version 2013-02-17-125637

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,8 @@ import datetime
 import time
 
 env.roledefs = {
-    'game': ['game.gps-strike.com'],
+#    'game': ['game.gps-strike.com'],
+    'game': ['ec2-46-137-16-128.eu-west-1.compute.amazonaws.com'],
 }
 
 def load_version():
@@ -23,21 +24,20 @@ def update_version():
     print "save %s to version file" % (version)
 
 def git_pull():
-    local('git pull git@github.com:Slach/gps-strike-server.git master')
+    local('git pull ssh+git://git@github.com/Slach/gps-strike-server.git master')
 
 
 def git_push():
     local('git add -A')
     with settings(warn_only=True):
         local('git commit -a -m "deploy version %s"' % ( version ))
-        local('git push git@github.com:LinguaLeo/analytics.git master')
+        local('git push ssh+git://git@github.com/Slach/gps-strike-server.git master')
 
 def upload_source():
     sudo('mkdir -p -m 0755 %s' % (homedir))
-    sudo('mkdir -p -m 0777 /var/log/')
-    put('*', homedir, use_sudo=True, mode=0755)
-    put('*/*', homedir, use_sudo=True, mode=0755)
-    sudo('rm -rfv %s/*.pyc' % (homedir))
+    sudo('mkdir -p -m 0777 /var/log/gps-strike')
+    put('*.py', homedir, use_sudo=True, mode=0755)
+    put('*.cfg', homedir, use_sudo=True, mode=0755)
     sudo('chmod 0755 %s/*.py' % (homedir))
     sudo('chown -R ubuntu:ubuntu %s' % (homedir))
 
